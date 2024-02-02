cfcb3b4202ca0ce28597343d8b1047c475318aa9
Aram <me@aram.sh>
2024-02-01 18:28:15 -0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,21 +19,21 @@ const Input = ({
   required = false,
   ...rest
 }: InputProps & Record<string, unknown>) => (
-    <div className="mb-4">
-      <label className="mb-2 block" htmlFor={`input__${id}`}>
-        {label}
-      </label>
-      <input
-          id={`input__${id}`}
-          className="w-full rounded-2xl border border-gray-300 px-3 py-2"
-          type="number"
-          value={value}
-          placeholder={placeholder}
-          onChange={onChange}
-          required={required}
-          {...rest}
-      />
-    </div>
+  <div className="mb-4">
+    <label className="mb-2 block" htmlFor={`input__${id}`}>
+      {label}
+    </label>
+    <input
+      id={`input__${id}`}
+      className="w-full rounded-2xl border border-gray-300 px-3 py-2"
+      type="number"
+      value={value}
+      placeholder={placeholder}
+      onChange={onChange}
+      required={required}
+      {...rest}
+    />
+  </div>
 );
 
 interface PizzaResponse {
@@ -74,89 +74,88 @@ const PizzaCalculator = () => {
   };
 
   return (
-      <div className="flex justify-center items-center h-screen">
-        <div className="mx-auto max-w-md rounded-xl bg-white p-14 shadow-lg text-black">
-          <h1 className="mb-4 text-3xl font-bold">Pizza Price Calculator</h1>
-          <p>
-            Ever wonder? Is this pizza cheaper than this pizza? This tool is a
-            short tool, that will calculate the price of a pizza by square inches.
+    <div className="flex justify-center items-center h-screen">
+      <div className="mx-auto max-w-md rounded-xl bg-white p-14 shadow-lg text-black">
+        <h1 className="mb-4 text-3xl font-bold">Pizza Price Calculator</h1>
+        <p>
+          Ever wonder? Is this pizza cheaper than this pizza? This tool is a
+          short tool, that will calculate the price of a pizza by square inches.
+        </p>
+        <form onSubmit={handleSubmit} className="py-5">
+          <Input
+            id="size"
+            label="Pizze Size (diameter in inches):"
+            placeholder="e.g. 12"
+            onChange={(e) => setPizzaSize(e.target.value)}
+            value={pizzaSize}
+            required={true}
+          />
+          <Input
+            id="cost"
+            label="Pizza Cost ($):"
+            placeholder="e.g. 12.99"
+            onChange={(e) => setPizzaCost(e.target.value)}
+            value={pizzaCost}
+            required={true}
+          />
+          <div className="mb-4">
+            <input
+              type="checkbox"
+              name="Calculate Crust"
+              id="crust_checkbox"
+              checked={calculateCrust}
+              onChange={(e) => setCalculateCrust(e.target.checked)}
+            />
+            <label htmlFor="crust_checkbox" className="pl-1">
+              I care about the crust
+            </label>
+          </div>
+
+          {calculateCrust && (
+            <Input
+              id="crustSize"
+              label="Size of crust (inches)"
+              placeholder="e.g. 1"
+              max={pizzaSize}
+              onChange={(e) => setCrustSize(e.target.value)}
+              value={crustSize}
+              required={true}
+            />
+          )}
+
+          <button
+            className="w-full rounded-xl bg-blue-700 px-4 py-2 text-white"
+            type="submit"
+            disabled={isLoading}
+          >
+            Calculate
+          </button>
+        </form>
+
+        {responseData.pricePerSquareInch != null && (
+          <p className="mt-4">
+            Price per square inch: ${responseData.pricePerSquareInch.toFixed(2)}
           </p>
-          <form onSubmit={handleSubmit} className="py-5">
-            <Input
-                id="size"
-                label="Pizze Size (diameter in inches):"
-                placeholder="e.g. 12"
-                onChange={(e) => setPizzaSize(e.target.value)}
-                value={pizzaSize}
-                required={true}
-            />
-            <Input
-                id="cost"
-                label="Pizza Cost ($):"
-                placeholder="e.g. 12.99"
-                onChange={(e) => setPizzaCost(e.target.value)}
-                value={pizzaCost}
-                required={true}
-            />
-            <div className="mb-4">
-              <input
-                  type="checkbox"
-                  name="Calculate Crust"
-                  id="crust_checkbox"
-                  checked={calculateCrust}
-                  onChange={(e) => setCalculateCrust(e.target.checked)}
-              />
-              <label htmlFor="crust_checkbox" className="pl-1">
-                I care about the crust
-              </label>
-            </div>
-
-            {calculateCrust && (
-                <Input
-                    id="crustSize"
-                    label="Size of crust (inches)"
-                    placeholder="e.g. 1"
-                    max={pizzaSize}
-                    onChange={(e) => setCrustSize(e.target.value)}
-                    value={crustSize}
-                    required={true}
-                />
-            )}
-
-            <button
-                className="w-full rounded-xl bg-blue-700 px-4 py-2 text-white"
-                type="submit"
-                disabled={isLoading}
-            >
-              Calculate
-            </button>
-          </form>
-
-          {responseData.pricePerSquareInch != null && (
-              <p className="mt-4">
-                Price per square inch: ${responseData.pricePerSquareInch.toFixed(2)}
-              </p>
-          )}
-          {responseData.pricePerSquareInchWithoutCrust != null && (
-              <p className="mt-4">
-                Price per square inch excluding crust: $
-                {responseData.pricePerSquareInchWithoutCrust.toFixed(2)}
-              </p>
-          )}
-          {responseData.percentOfPizzaIsCrust != null && (
-              <p className="mt-4">
-                Percent of pizza is crust:{" "}
-                {(responseData.percentOfPizzaIsCrust * 100).toFixed(2)}%
-              </p>
-          )}
-          {responseData.payForCrust != null && (
-              <p className="mt-4">
-                You pay just for crust: $
-                {responseData.payForCrust.toFixed(2)}
-              </p>
-          )}
-        </div>
+        )}
+        {responseData.pricePerSquareInchWithoutCrust != null && (
+          <p className="mt-4">
+            Price per square inch excluding crust: $
+            {responseData.pricePerSquareInchWithoutCrust.toFixed(2)}
+          </p>
+        )}
+        {responseData.percentOfPizzaIsCrust != null && (
+          <p className="mt-4">
+            Percent of pizza is crust:{" "}
+            {(responseData.percentOfPizzaIsCrust * 100).toFixed(2)}%
+          </p>
+        )}
+        {responseData.payForCrust != null && (
+          <p className="mt-4">
+            You pay just for crust: ${responseData.payForCrust.toFixed(2)}
+          </p>
+        )}
       </div>
+    </div>
   );
 };
 
